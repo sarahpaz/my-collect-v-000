6f6b5d2f25dedb583fba3c32a3c640c042038b44
Sarah Paz <sarah.paz@gmail.com>
2018-12-21 05:05:51 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,10 @@
 def my_collect(array)
   i = 0
   array = []
-  
+
   while i < array.length do |item|
-    collection << array[i]
+    collection << array[item]
     i += 1
   end
   collection
 end
-
